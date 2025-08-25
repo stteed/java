ad7252396d6f53ab11f72114999120d7bbb92da6
stteed <kec6813@gmail.com>
2025-08-24 22:17:06 -0700

    aa

---
+++ chap02/src/sec02exam03/LongExamplejava.java
@@ -0,0 +1,16 @@
+package sec02exam03;
+
+public class LongExamplejava {
+
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		long var3 = 100000000000L;
+		long var4 = 1000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	}
+
+}
---
+++ chap02/src/sec02exam06/EscapeExamplejava.java
@@ -0,0 +1,13 @@
+package sec02exam06;
+
+public class EscapeExamplejava {
+
+	public static void main(String[] args) {
+		System.out.println("번호\t이름t직업");
+		System.out.println("행 단위 출력\n");
+		System.out.println("행 단위 출력\n");
+		System.out.println("우리는 \" 개발자\" 입니다.");
+		System.out.println("봄\\여름\\가을\\겨울");
+	}
+
+}
